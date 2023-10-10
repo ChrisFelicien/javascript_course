2518a2555a49c0e6614513eb4f514cebbcd3c899
Chris Felicien <f.ilungakalenga@gmail.com>
2023-10-10 15:47:39 +0200

    set view change high score

--- 01.Check_my_number/script.js
+++ 01.Check_my_number/script.js
@@ -1,18 +1,26 @@
 'use strict';
 
+let CurrentScore = 20;
 const message = document.querySelector('.message');
 const number = document.querySelector('.number');
 const score = document.querySelector('.score');
 const checkBtn = document.querySelector('.check');
+const scoreView = document.querySelector('.score');
+const userHighscore = document.querySelector('.highscore');
 const winNumber = Math.trunc(Math.random() * 20) + 1;
 
 const checkTwoNumberEquality = (userNumber, machineNumber) => {
   if (userNumber > machineNumber) {
+    CurrentScore = CurrentScore - 1;
+    score.textContent = CurrentScore;
     return (message.textContent = 'Your Number is high');
   } else if (userNumber < machineNumber) {
+    CurrentScore = CurrentScore - 1;
+    score.textContent = CurrentScore;
     return (message.textContent = 'Your Number is low');
   }
   number.textContent = winNumber;
+  userHighscore.textContent = CurrentScore;
   return (message.textContent = 'Congratulation you won the game');
 };
 
